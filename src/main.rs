ec76de8f2320c5b6387ece09d7195a195a6b7779
Trip Volpe <trip.volpe@gmail.com>
2016-05-13 21:18:56 -0700

    Add stub http handler with iron

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Piss off!")))
+    }
+
+    println!("Starting HTTP server on port 1337...");
+    Iron::new(hello_world).http("localhost:1337").unwrap();
 }
